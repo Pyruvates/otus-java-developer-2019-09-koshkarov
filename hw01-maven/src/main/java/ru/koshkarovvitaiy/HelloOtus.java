457334860bcf435751da5c753f07a7791f37d83b
Vitaliy Koshkarov <46346811+Pyruvates@users.noreply.github.com>
2019-10-07 10:29:21 +0300

    Revert "Master"

---
+++ hw01-maven/src/main/java/ru/koshkarovvitaiy/HelloOtus.java
@@ -0,0 +1,8 @@
+package ru.koshkarovvitaiy;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println("Hello, Otus!");
+        System.out.println("I finally defeated Maven!");
+    }
+}
